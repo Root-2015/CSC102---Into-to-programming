010be535730579d515e48f6ad320f25709c499e9
Root <gangestadtyler@gmail.com>
2025-10-19 08:51:56 -0700

    Added user validation

--- dice code.js
+++ dice code.js
@@ -1,5 +1,5 @@
-     // Functions alow use to organize our cod and make it reusable. want it as descriptive as possible.
-     function playDice(){
+    // Functions alow use to organize our cod and make it reusable. want it as descriptive as possible.
+    function playDice(){
         console.log("playDice was called");
         //roll die1
         let roll1 = rollDie();
@@ -8,7 +8,12 @@
         let sum = roll1 + roll2;
         let result = "";
         // Lose if they roll less than 41.
-        if (sum < 41){
+        //this means it has to be within 30- 90 to get a tie
+        if (sum >=41 && sum < 90){
+            console.log("Tie")
+            result = "you tied";
+        }
+        else if (sum < 41){
             console.log("you lose");
             result = "you lose";
         }
@@ -16,11 +21,7 @@
             console.log("you Win!")
             result = "you win";
         }
-        //this means it has to be within 30- 90 to get a tie
-        else if (sum >=41 && sum < 90){
-            console.log("Tie")
-            result = "you tied";
-        }
+
         // sets this id to the Sum variable
         document.getElementById("divSum").textContent = "Sum = " + sum;
         document.getElementById("divRoll1").textContent = "Roll1 = " + roll1;
@@ -28,10 +29,41 @@
         document.getElementById("divResult").textContent = "Result = " + result;
     }
        //Function for the random die
-        function rollDie(){
-            //this generates a random number then times it by fifty to make a 50 sided die.
-            let die = Math.random()*50 // *= multiply by
-            //round the rumber to whole number
-            return Math.ceil(die)
+    function rollDie(){
+         //this generates a random number then times it by fifty to make a 50 sided die.
+        let die = Math.random()*50 // *= multiply by
+        //round the rumber to whole number
+        return Math.ceil(die)
         
-        }  
+    }  
+    function validateForm()
+    {
+        let FullName = ""
+        let FirstName = document.getElementById("txtFirstName").value.trim();
+        let LastName = document.getElementById("txtLastName").value.trim();
+        let Zipcode = document.getElementById("txtZipcode").value.trim();
+        console.log(FirstName)
+        console.log(LastName)
+        console.log(Zipcode)
+        message = ""
+        // Valadation - we need to make sure that first name + " " + last name is less than 20 charectors.
+        // Zip needs to be 5 digits
+        FullName = FirstName + " " + LastName
+        console.log(FullName)
+        if (FullName.length > 20 || FullName.length == 1){
+            //invaled name
+            message = "Name was spelt incorectly or done wrong."
+        }
+        else {
+            code = true
+        }
+        if (Zipcode.length != 5){
+            message = message + " Your zip code is 5 charectors"
+        }
+        else (code = true)
+        {
+        //Secrete message if players do it right
+        secrete = "You did it "+FirstName}
+        console.log(message || secrete) 
+        document.getElementById("divMessage").textContent = message;
+    }
